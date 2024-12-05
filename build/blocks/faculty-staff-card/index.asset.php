ad5123bf2305d4130fc324421f6f77bb1ac6bef7
Ian Watkins <iwatkins@highpoint.edu>
2024-12-05 15:03:51 -0500

    clean build for new version 0.3.0

--- build/blocks/faculty-staff-card/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '5e25e82b0a4ddc864f1a');
--- build/blocks/testing-multipost/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '32b42cedd8a0e401688f');
