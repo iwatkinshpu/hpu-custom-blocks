f20bb7f03c39eb6da3098303381ca1519eb28657
Ian Watkins <iwatkins@highpoint.edu>
2024-12-05 14:42:24 -0500

    provided localized data and assets to editor

--- hpu-custom-blocks.php
+++ hpu-custom-blocks.php
@@ -25,14 +25,39 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
+
+/**
+ * Enqueue plugin settings for availability within blocks
+ */
+
+function hpu_custom_blocks_localize_block_data( $script_handle ) {
+	$data = [
+		'directoryHome' => 1, // To be pulled dynamically later
+		'assetPath'    => plugin_dir_url( __FILE__ ) . 'assets/',
+	];
+	
+	// Localize the data to the block script
+	wp_localize_script( $script_handle, 'HPUCustomBlocksData', $data );
+}
+
 function hpu_custom_blocks_block_init() {
+
+	// register the blocks
 	$build_dir = plugin_dir_path( __FILE__ ) . 'build/blocks/';
+
 	foreach ( glob( $build_dir . '*/' ) as $block_dir ) {
 		if ( ! file_exists( $block_dir . 'block.json' ) ) {
 			error_log( 'hpu-custom-blocks ### failed to register block at ' . $block_dir . ' --- no block.json file found' );
-			return;
+			continue;
 		}
-		register_block_type( $block_dir );
+
+		// register block
+		$block = register_block_type( $block_dir );
+
+		// localise block data script
+		if ( isset( $block->editor_script ) && $block->editor_script ) {
+			hpu_custom_blocks_localize_block_data( $block->editor_script );
+		}
 	}
 }
 add_action( 'init', 'hpu_custom_blocks_block_init' );
@@ -44,18 +69,3 @@ function hpu_register_custom_endpoints() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/rest-api.php';
 }
 add_action( 'init', 'hpu_register_custom_endpoints' );
-
-function maybe_add_acf_to_api() {
-    // Loop through all public post types
-	if ( function_exists( 'get_fields' ) && current_user_can( 'edit_posts' ) ) {
-		foreach ( get_post_types( [ 'public' => true ], 'objects' ) as $post_type ) {
-			register_rest_field( $post_type->name, 'acf', [
-				'get_callback' => function( $object ) {
-					return get_fields( $object['id'] );
-				},
-				'schema' => null,
-			] );
-		}
-	}
-};
-add_action( 'rest_api_init', 'maybe_add_acf_to_api' );
