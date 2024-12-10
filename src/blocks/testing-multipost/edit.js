e1260f0774c8ebaa59a47e46b1e9ac0041585444
Ian Watkins <iwatkins@highpoint.edu>
2024-12-10 16:18:00 -0500

    updated blocks for testing with components updates

--- src/blocks/testing-multipost/edit.js
+++ src/blocks/testing-multipost/edit.js
@@ -1,38 +1,65 @@
 import { useState, useEffect } from '@wordpress/element';
 import { SelectControl } from '@wordpress/components';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
-import { PostSearchControls } from '@hpu-wp/components';
+import { PostSearchControls, SelectBlogControls } from '@hpu-wp/components';
 import './editor.scss';
 
 export default function Edit( { attributes, setAttributes } ) {
-	const { postArray = [], postType } = attributes;
-	console.log( 'attributes', attributes );
+	const { postArray = [], postType, blogID } = attributes;
+	const [ filter, setFilter ]                = useState( {} );
+
+	useEffect( () => {
+		setFilter( {
+			postType: postType || null,
+			blogID: blogID || null,
+		} )
+	}, [ postType, blogID ] );
+
+	console.log( filter );
+
+	const resetPosts = ( reason='' ) => {
+		console.log( 'resetPosts', reason );
+		setAttributes( { postArray: [] } );
+	}
 
 	return (
 		<>
 			<InspectorControls>
-				<SelectControl
-					label='Select Post Type'
-					value={ postType || 'posts' }
-					onChange={ ( value ) => { 
-						setAttributes( { postType: value, postArray: [] } );
+				<SelectBlogControls
+					blogID={ blogID }
+					onChange={ ( value ) => {
+						resetPosts( 'blogID: ' + blogID );
+						setAttributes( { blogID: value } )
 					} }
-					options={ [
-						{ value: 'posts', label: 'Posts' },
-						{ value: 'pages', label: 'Pages' },
-						{ value: 'menu-items', label: 'Menu Items' },
-					] }
-					__nextHasNoMarginBottom
 				/>
+				<div className='hpu-multipost-testing-select-wrapper'>
+					<SelectControl
+						label='Select Post Type'
+						value={ postType || 'posts' }
+						onChange={ ( value ) => {
+							resetPosts( 'postType: ' + postType );
+							setAttributes( { postType: value } );
+						} }
+						options={ [
+							{ value: 'posts', label: 'Posts' },
+							{ value: 'pages', label: 'Pages' },
+							{ value: 'menu-items', label: 'Menu Items' },
+						] }
+						__nextHasNoMarginBottom
+					/>
+				</div>
 				<PostSearchControls
 					postArray={ postArray }
-					postType={ postType }
+					postType={ filter?.postType }
+					blogID={ filter?.blogID }
 					wpNonce={ window.HPUCustomBlocksData.restNonce }
-					onChange={ ( value ) => { setAttributes( { postArray: value } ) } }
+					onChange={ ( value ) => {
+						setAttributes( { postArray: value } )
+					} }
 				/>
 			</InspectorControls>
 			<div { ...useBlockProps() }>
-				Testing MultiPost PostSearchControls component
+				<p>Testing MultiPost PostSearchControls component</p>
 				{ Array.isArray( postArray ) && postArray?.map( ( post, index ) => (
 					<p key={ index }>{ post }</p>
 				) ) }
