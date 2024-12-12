ef15f8cd4b1a35ae11054e5919806d27c17c1462
Ian Watkins <iwatkins@highpoint.edu>
2024-12-12 09:15:40 -0500

    increment version to 0.4.1 and clean build

--- build/blocks/testing-multipost/index.asset.php
+++ build/blocks/testing-multipost/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '97b987de1d146b70fb74');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'a4b47ba468584453a7cd');
