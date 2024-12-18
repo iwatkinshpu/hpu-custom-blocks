3f322b12663b54139c4161da24cad275b96ee6db
Ian Watkins <iwatkins@highpoint.edu>
2024-12-18 16:47:57 -0500

    clean build

--- build/blocks/faculty-staff-card/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '076a6771bee82cc8c80d');
--- build/blocks/testing-multipost/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'c9b11f928902af088953');
